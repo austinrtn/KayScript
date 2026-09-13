7170ba7d60ae605ab978edd5466192c407b12495
Dogmatic Polack <kolowemployment@gmail.com>
2026-09-12 20:46:55 -0400

    Whole new woooorld

--- src/main.zig
+++ src/main.zig
@@ -5,14 +5,31 @@ const Io = std.Io;
 const kayscript = @import("kayscript");
 
 pub fn main(init: std.process.Init) !void {
-    const res = try lsblk(init.gpa, init.io);
-    defer init.gpa.destroy(res);
-
+    const allocator = init.arena.allocator();
+    const io = init.io;
+    
+    var stdin_buf: [1024]u8 = undefined;
+    var reader = std.Io.File.stdin().reader(io, &stdin_buf);
+    const stdin = &reader.interface;
+    
+    var stdout_buf: [1024]u8 = undefined;
+    var writer = std.Io.File.stdout().writer(io, &stdout_buf);
+    const stdout = &writer.interface;
+    cls(stdout);
+    
+    const res = try lsblk(allocator, io);
     const entries = res.blockdevices;
 
-    for(entries) |ent| {
-        std.debug.print("{s}\n", .{ent.name});
+    try stdout.writeAll("Select a Block Device: \n");
+    for(entries, 0..) |ent, i| {
+        try stdout.print("{d}: {s}\n", .{i, ent.name});
     }
+    try stdout.flush();
+
+    const resp = try stdin.takeDelimiterExclusive('\n');
+    const idx = try std.fmt.parseInt(usize, resp, 0);
+    try stdout.print("You etnered: {s}", .{entries[idx].name});
+    try stdout.flush();
 }
 
 const LsblkEntry = struct {
@@ -26,12 +43,15 @@ const LsblkOutput = struct {
     blockdevices: []LsblkEntry,
 };
 
-fn lsblk(gpa: Allocator, io: Io) !*LsblkOutput {
-    const cmd = try std.process.run(gpa, io, .{
+fn cls(stdout: *std.Io.Writer) void {
+    stdout.writeAll("\x1b[2J\x1b[H") catch @panic("Unable to clear screen");
+    stdout.flush() catch @panic("Unable to clear screen");
+}
+
+fn lsblk(allocator: Allocator, io: Io) !*LsblkOutput {
+    const cmd = try std.process.run(allocator, io, .{
         .argv = &.{ "lsblk", "--json", "--output", "NAME,TYPE,SIZE,ID-LINK" },
     });
-    defer gpa.free(cmd.stderr);
-    defer gpa.free(cmd.stdout);
 
     getCmdError(cmd) catch |err| {
         std.debug.print("{s}\n", .{cmd.stderr});
@@ -40,13 +60,12 @@ fn lsblk(gpa: Allocator, io: Io) !*LsblkOutput {
 
     const parsed = try std.json.parseFromSlice(
         LsblkOutput,
-        gpa, 
+        allocator, 
         cmd.stdout, 
         .{.ignore_unknown_fields = true},
     );
-    defer parsed.deinit();
 
-    const res_ptr = try gpa.create(LsblkOutput);
+    const res_ptr = try allocator.create(LsblkOutput);
     res_ptr.* = parsed.value;
     return res_ptr;
 }
